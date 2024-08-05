92f89bf0664612b4a636777822fbf5eba476810d
amatrelan <github.afterglow462@passmail.net>
2024-08-05 09:25:21 +0300

    Remove mini, and bindings for buffer things

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,32 +1,6 @@
 --- @type LazySpec
 return { -- Collection of various small independent plugins/modules
   {
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Visual indent line, gotta use python at work so this is pretty much
-      -- must have, also yml...
-      -- require('mini.indentscope').setup({})
-
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
-      -- require('mini.ai').setup({
-      --   n_lines = 500,
-      -- })
-
-      -- local statusline = require('mini.statusline')
-      -- statusline.setup({ use_icons = vim.g.have_nerd_font })
-      --
-      -- ---@diagnostic disable-next-line: duplicate-set-field
-      -- statusline.section_location = function()
-      --   return '%2l:%-2v'
-      -- end
-    end,
-  },
-  {
     'echasnovski/mini.surround',
     opts = {
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
--- lua/plugins/tabbar.lua
+++ lua/plugins/tabbar.lua
@@ -19,6 +19,36 @@ return {
       desc = 'Previous Buffer',
       mode = 'n',
     },
+    {
+      '<leader>bd',
+      '<cmd>BufferClose<cmd>',
+      desc = 'Buffer Close',
+    },
+    {
+      '<leader>bsn',
+      '<cmd>BufferOrderByName<cmd>',
+      desc = 'Buffer order by Name',
+    },
+    {
+      '<leader>bsd',
+      '<cmd>BufferOrderByDirectory<cmd>',
+      desc = 'Buffer order by Directory',
+    },
+    {
+      '<leader>bsl',
+      '<cmd>BufferOrderByLanguage<cmd>',
+      desc = 'Buffer order by Language',
+    },
+    {
+      '<leader>bsw',
+      '<cmd>BufferOrderByWindowNumber<cmd>',
+      desc = 'Buffer order by Window Number',
+    },
+    {
+      '<leader>bsb',
+      '<cmd>BufferOrderByBufferNumber<cmd>',
+      desc = 'Buffer order by Buffer Number',
+    },
   },
   opts = {},
 }
