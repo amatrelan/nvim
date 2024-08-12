6d3dbafc432bbe7cce2c07fbb44e09bb640874cb
amatrelan <github.afterglow462@passmail.net>
2024-08-12 07:55:09 +0300

    disable mason-dap in nixos

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,12 +1,16 @@
 return {
   {
+    'jay-babu/mason-nvim-dap.nvim',
+    enabled = function()
+      local obj = vim.system({ 'rg', '-i', 'nixos', '/etc/os-release' }, { text = true }):wait()
+      return obj.code ~= 0
+    end,
+  },
+  {
     'williamboman/mason.nvim',
     enabled = function()
       local obj = vim.system({ 'rg', '-i', 'nixos', '/etc/os-release' }, { text = true }):wait()
       return obj.code ~= 0
     end,
-    opts = {
-      ensure_installed = {},
-    },
   },
 }
