180e312eb56f4f92c21f8d34bdc8ab79c91d3a4d
amatrelan <github.afterglow462@passmail.net>
2024-10-08 22:12:23 +0300

    remove useless opts

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -19,9 +19,6 @@ return {
       local obj = vim.system({ 'rg', '-i', 'nixos', '/etc/os-release' }, { text = true }):wait()
       return obj.code ~= 0
     end,
-    opts = {
-      ensure_installed = {},
-    },
   },
   {
     'zbirenbaum/copilot.lua',
