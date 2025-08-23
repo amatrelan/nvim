79649f72a726e36e3134d43ea8358c64191653ef
amatrelan <github.afterglow462@passmail.net>
2025-08-23 10:15:24 +0300

    neovide and use nu if windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,18 @@ vim.g.lazyvim_python_ruff = 'ruff'
 vim.g.autoformat = false
 
 vim.g.snacks_animate = false
+
+if vim.fn.has("windows") then
+  vim.o.shell = "nu"
+end
+
+-- Neovide
+if vim.g.neovide then
+  local default_path = vim.fn.expand("~/")
+  vim.api.nvim_set_current_dir(default_path)
+  vim.o.guifont = 'MonaspiceKr Nerd Font:16h'
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_scroll_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_short_animation_length = 0
+end
